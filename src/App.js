3c89c8b7ad62acde51651088be4c8bb328cce232
Parth Sagar <kallathiyaparth744@gmail.com>
2023-02-22 21:22:23 -0800

    design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './App.scss';
 
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import Routes from './config/Routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        Parth
-      </h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <>
+                    <Header {...props}/>
+                    <Routes/>
+                    <Footer/>
+                </>
+            )}/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
